cda495e00d2f9d9f602cc843c32cd6589f26e675
Shyamptdr0 <anu.shyam.patidar@gmail.com>
2025-06-13 16:54:44 +0530

    pta nhiiiiii

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ const app = express()
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: process.env.CLIENT_BASE_URL,
+    origin: "https://kapada-store-1.onrender.com",
     methods: "GET,POST,PUT,DELETE",
     allowedHeaders: [
         "Content-type",
